e6a87ff0f8b845c813ba8122dbd26e00e5c3dd4c
annamdevaranyv-maker <annamdevaranyv@gmail.com>
2025-11-24 10:31:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,42 +5,41 @@ from file_manager import save_game, load_game, has_save, remove_save, is_number
 def play():
     if has_save():
         ans = input("Saved game found — load it? (y/n): ").strip().lower()
-        if ans in ("y", "yes"):
+        if ans in ("y", "yes"):                                         # gets you your previous game
             board = load_game()
             if board is None:
                 board = create_board()
         else:
             board = create_board()
     else:
-        board = create_board()
+        board = create_board()                              # else gets u a new game
 
     moves = 0
-
     while True:
         display_board(board)
-        print("Commands: 'r row col', 'f row col', 'save', 'load', 'quit'")
+        print("Commands: 'r row col', 'f row col', 'save', 'load', 'quit'")                 # to ask user to give instructions
         raw = input("Enter: ").strip().lower()
 
         if raw == "quit":
             ans = input("Save game before exit? (y/n): ").strip().lower()
             if ans in ("y", "yes"):
-                save_game(board)
+                save_game(board)                                      # exits the game
             print("see u later...🙂")
             break
 
         if raw == "save":
-            save_game(board)
+            save_game(board)                    # saves the game
             continue
 
         if raw == "load":
             nb = load_game()
-            if nb:
+            if nb:                                  # brings back the last game
                 board = nb
             continue
 
         parts = raw.split()
         if len(parts) != 3:
-            print("Bad input. Example: r 3 4")
+            print("Bad input. Example: r 3 4")          # corrects the user for the input
             continue
 
         action, sr, sc = parts
@@ -58,14 +57,14 @@ def play():
         if action == "r":
             if board[r][c].mine:
                 reveal_all_mines(board)
-                display_board(board, show_mines=True)
+                display_board(board, show_mines=True)                       # game over if the bomb is selected
                 print(f"💥 Boom! You hit a mine. Game Over. Moves: {moves}")
                 remove_save()
                 break
             reveal_cell(board, r, c)
             if check_win(board):
                 reveal_all_mines(board)
-                display_board(board, show_mines=True)
+                display_board(board, show_mines=True)                   # won the game if all the cells except the mine cells are selected
                 print(f"🏆 You cleared the field in {moves} moves!")
                 remove_save()
                 break
@@ -75,6 +74,6 @@ def play():
 
         else:
             print("Unknown action. Use 'r' or 'f'.")
-
 if __name__ == "__main__":
     play()
+
